0a9bb9d25403e1eec32b5f7e8e03041d2d44d22e
wiebejordan <wiebe.jordan@yahoo.com>
2020-11-12 12:47:11 -0700

    server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
+require ('dotenv').config();
+
 const express = require('express'),
-      authCtrl = require('./authController'),
-      app = express();
+      app = express(),
+      massive = require('massive'),
+      {SERVER_PORT, SESSION_SECRET, DB_URI} = process.env,
+      authCtrl = require('./authController');
 
 
 
 app.use(express.json())
 
-app.listen(5150, () => console.log('server is running on 5150'))
+app.listen(SERVER_PORT, () => console.log(`server is running on ${SERVER_PORT}`));
