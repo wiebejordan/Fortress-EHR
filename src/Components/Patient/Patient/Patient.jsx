2135d8500dbe2893dabb99f5110e86915ce36416
wiebejordan <wiebe.jordan@yahoo.com>
2020-12-03 15:28:48 -0700

    spanish to english menu functions

--- src/Components/Patient/Patient/Patient.jsx
+++ src/Components/Patient/Patient/Patient.jsx
@@ -1,94 +1,97 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import {connect, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import {Loader, Menu} from 'semantic-ui-react';
 import PatientMenu from '../PatientMenu/PatientMenu'
 
 
-const mapStateToProps = (reduxState) => reduxState;
 
- const Patient = (props) => {
-   const [patientID, setPatientID] = useState(null),
-         [firstnm, setFirstNm] = useState(''),
-         [lastnm, setLastNm] = useState(''),
-         [birthdts, setBirthdts] = useState(''),
-         [genderdsc, setGenderdsc] = useState(''),
-          [hispanicflg, setHispanicflg] = useState(''),
-          [ethnicitydsc, setEthnicitydsc] = useState(''),
-          [race01dsc, setRace01dsc] = useState(''),
-          [race02dsc, setRace02dsc] = useState(''),
-          [activeflg, setActiveflg] = useState(''),
-          [loading, setLoading] = useState(true),
-         user = useSelector(state => state.authReducer),
-         history = useHistory();
 
-  useEffect(() => {
-    
-    if(props.match.params.patientid){
-      axios.get(`/api/patient/${props.match.params.patientid}`)
-      .then(res => {
-        setPatientID(res.data[0].patientid);
-        setFirstNm(res.data[0].firstnm);
-        setLastNm(res.data[0].lastnm);
-        setBirthdts(res.data[0].birthdts);
-        setGenderdsc(res.data[0].genderdsc);
-        setHispanicflg(res.data[0].hispanicflg);
-        setEthnicitydsc(res.data[0].ethnicitydsc);
-        setRace01dsc(res.data[0].race01dsc);
-        setRace02dsc(res.data[0].race02dsc);
-        setActiveflg(res.data[0].activeflg);
-        setLoading(false);
-      })
-    }
-  }, [])
 
-  useEffect(() => {
-    if(!user.user.email){
-      history.push('/')}
-  })
+ const Patient = () => {
+  const [item, setItem] = useState('overview'),
+  lang = useSelector(state => state.languageReducer.english)
 
-  if(loading){
-    return <div className='loader-container'>
-           
-    <Loader size='massive' active />
+  const handleItemClick = (e, { name }) => setItem(name);
+
+  console.log(item)
   
- </div> 
-  }
   
-  return(<div>
-    <PatientMenu/>
-    
-    {props.languageReducer.english === true 
-    ?(
-    <>
-    <p><b>Id:</b> {patientID}</p>
-    <p><b>Name:</b> {lastnm}, {firstnm}</p> 
-    <p><b>Date of Birth:</b> {birthdts}</p>
-    <p><b>Gender:</b> {genderdsc}</p>
-    {/* <p><b>Height:</b> {patient.height}</p>
-    <p><b>Weight:</b> {patient.weight}</p> */}
-    {/* <p><b>Allergies:</b> {patient.allergies}</p>
-    <p><b>Patient History:</b> {patient.patientHistory}</p>
-    <p><b>Active Problems:</b> {patient.activeProblems}</p>
-    <p><b>Medications:</b> {patient.medications}</p> */}
-    </>
-    )
-    : 
-    <>
-    <p><b>Id:</b> {patientID}</p>
-    <p><b>Nombre:</b> {lastnm}, {firstnm}</p> 
-    <p><b>Fecha de Nacimiento:</b> {birthdts}</p>
-    <p><b>Género:</b> {genderdsc}</p>
-    {/* <p><b>Altura:</b> {patient.height}</p>
-    <p><b>Peso:</b> {patient.weight}</p>
-    <p><b>Alergias:</b> {patient.allergies}</p>
-    <p><b>Historial del Paciente:</b> {patient.patientHistory}</p>
-    <p><b>Problemas Activos:</b> {patient.activeProblems}</p>
-    <p><b>Medicamentos:</b> {patient.medications}</p> */}
-    </>}
-  </div>
+  return(
+    <div>
+      
+      {lang === true
+      ?
+      <Menu  vertical>
+        <Menu.Item
+          name='overview'
+          active={item === 'overview' || item === 'visión de conjunto'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='patient history'
+          active={item === 'patient history' || item === 'historial del paciente'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='active problems'
+          active={item === 'active problems' || item === 'problemas activos'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='medications'
+          active={item === 'medications' || item === 'medicamentos'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='immunizations'
+          active={item === 'immunizations' || item === 'inmunizaciones'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='visualization'
+          active={item === 'visualization' || item === 'visualizaciones'}
+          onClick={handleItemClick}
+        />
+      </Menu>
+      // ////////////////////////////spanish menu////////////////////////////////////
+      :
+      <Menu  vertical>
+        <Menu.Item
+          name='visión de conjunto'
+          active={item === 'overview' || item === 'visión de conjunto'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='historial del paciente'
+          active={item === 'patient history' || item === 'historial del paciente'}
+          onClick={handleItemClick}
+        />
+          <Menu.Item
+            name='problemas activos'
+            active={item === 'active problems' || item === 'problemas activos'}
+            onClick={handleItemClick}
+          />
+        <Menu.Item
+          name='medicamentos'
+          active={item === 'medications' || item === 'medicamentos'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='inmunizaciones'
+          active={item === 'immunizations' || item === 'inmunizaciones'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='visualizaciones'
+          active={item === 'visualization' || item === 'visualizaciones'}
+          onClick={handleItemClick}
+        />
+      </Menu>
+      }
+      </div>
   )
 }
 
-export default connect(mapStateToProps)(Patient);
+export default Patient
