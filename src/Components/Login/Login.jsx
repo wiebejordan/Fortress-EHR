89bd8647c6a76105faf12c0cd3259f2d85be05d8
wiebejordan <wiebe.jordan@yahoo.com>
2020-12-16 16:20:29 -0700

    Fortress description

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {useSelector, useDispatch} from 'react-redux';
+import { Menu, Segment, Grid, Image, Container, Divider } from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
 import '../../styles/style.scss'
 
@@ -57,7 +58,7 @@ const Login = (props) => {
     {state.english === true
     ?
     <div className='login-box'>
-      <h4>Welcome to FORTRESS Electronic Medical Records!</h4>
+      <h4>Welcome to FORTRESS Electronic Health Records!</h4>
       <form onSubmit={handleLogin}>
       <input onChange={(e) => handleUserInput(e.target.value)} placeholder='email'/>
       <input onChange={(e) => handlePassInput(e.target.value)} placeholder='password' type='password'/>
@@ -69,7 +70,7 @@ const Login = (props) => {
     </div>
     :
     <div className='login-box'>
-      <h4>Bienvenido a FORTRESS Registros Médicos Electrónicos!</h4>
+      <h4>Bienvenido a FORTRESS Registros de Salud Electrónicos!</h4>
       <form onSubmit={handleLogin}>
       <input onChange={(e) => handleUserInput(e.target.value)} placeholder='correo electrónico'/>
       <input onChange={(e) => handlePassInput(e.target.value)} placeholder='contraseña' type='password'/>
@@ -84,11 +85,31 @@ const Login = (props) => {
     </div>
     
     <div className='about-container'>
-      <h1>What is Fortress EMR?</h1>
-
-      <p>Fortress EMR was born from the belief that every child, no matter their circumstances, deserves the best medical care possible. We believe this is especially true for children in orphanages, foster homes, and impoverished areas. In situations like these, where children receive much of their medical care from incoming and rotating short-term teams, it is essential for these teams to have access to each child's medical records so they can maximize their care giving efforts. </p>
-
-      <p>Fortress EMR provides custom, multilingual electronic medical records in an easy to use and secure platform.  </p>
+      <h1>What is Fortress EHR?</h1>
+      <div style={{margin: '20px'}}>
+      <Container >
+      
+      <Grid verticalAlign='middle' celled='internally' stackable>
+        <Grid.Row>
+          <Grid.Column width={10}>
+      <p>Fortress EHR was born from the belief that every child, no matter their circumstances, deserves the best medical care possible. We believe this is especially true for children in orphanages, foster homes, and impoverished areas. In situations like these, where children receive much of their medical care from incoming and rotating short-term teams, it is essential for these teams to have access to each child's medical records so they can maximize their care giving efforts. </p>
+      </Grid.Column>
+        <Grid.Column width={6}>
+          <Image size='massive' src='https://images.unsplash.com/photo-1547082688-9077fe60b8f9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80'/>
+        </Grid.Column>
+      </Grid.Row>
+      
+      <Grid.Row>
+        <Grid.Column width={4}>
+        <Image size='massive' circular src='https://i.imgur.com/vz5KfC0.jpg?1'/>
+        </Grid.Column>
+        <Grid.Column width={12}>
+      <p>Fortress EHR provides custom, multilingual electronic medical records in an easy to use and secure platform. But thats not even the best part! Because Fortress is a non-profit run completely by donations, Fortress EHR is completely free to the communities who need it! </p>
+      </Grid.Column>
+      </Grid.Row>
+      </Grid>
+      </Container>
+      </div>
     </div>
     </div>
   )
