e807aeb4798fdbaabb997e3193678e6f6c97e4d6
wiebejordan <wiebe.jordan@yahoo.com>
2020-12-22 20:15:33 -0700

    encounter table

--- src/Components/Patient/ActiveProblems/EncounterHistory.jsx
+++ src/Components/Patient/ActiveProblems/EncounterHistory.jsx
@@ -43,7 +43,41 @@ const EncounterHistory = (props) => {
             </thead>
 
         {encounterArr.map((encounter) => 
-            <tr className='enc-tr' key={encounter.encounterid} value={encounter}>
+            <tbody className='enc-tr' key={encounter.encounterid} value={encounter}>
+            
+            <tr className='enc-tr'>
+            <td className='enc-td'>{encounter.encounterdts.substr(0, 10)}</td>
+            <td className='enc-td'>{encounter.weight_lbs}</td>
+            <td className='enc-td'>{encounter.height_inch}</td>
+            <td className='enc-td'>{encounter.systolic_bp}</td>
+            <td className='enc-td'>{encounter.diastolic_bp}</td>
+            <td className='enc-td'>{encounter.heart_rate}</td>
+            <td className='enc-td'>{encounter.respirations_min}</td>
+            <td className='enc-td'>{encounter.commenttxt}</td>
+            </tr>
+            </tbody>
+        )}
+          </table>
+      </div>
+      // ////////////////////////////spanish menu////////////////////////////////////
+      :
+      <div>
+          <table className='enc-table'>
+            <thead className='enc-head'>
+            <tr className='enc-tr'>
+              <th className='enc-th'>Fecha del Encuentro</th>
+              <th className='enc-th'>Peso (lbs)</th>
+              <th className='enc-th'>Altura (inch)</th>
+              <th className='enc-th'>Presión Arterial Sistólica</th>
+              <th className='enc-th'>Presión Arterial Diastólica</th>
+              <th className='enc-th'>Ritmo Cardiaco</th>
+              <th className='enc-th'>Respiraciones Por Minuto</th>
+              <th className='enc-th'>Notas de Encuentro</th>
+            </tr>
+            </thead>
+
+        {encounterArr.map((encounter) => 
+            <tbody className='enc-tr' key={encounter.encounterid} value={encounter}>
               
             <td className='enc-td'>{encounter.encounterdts.substr(0, 10)}</td>
             <td className='enc-td'>{encounter.weight_lbs}</td>
@@ -54,25 +88,9 @@ const EncounterHistory = (props) => {
             <td className='enc-td'>{encounter.respirations_min}</td>
             <td className='enc-td'>{encounter.commenttxt}</td>
 
-            </tr>
+            </tbody>
         )}
           </table>
-      </div>
-      // ////////////////////////////spanish menu////////////////////////////////////
-      :
-      <div>Historia De Encuentros
-        {encounterArr.map((encounter) => 
-          <div key={encounter.encounterid} value={encounter}>
-            <p>Fecha de Encuentro: {encounter.encounterdts}</p>
-            <p>Peso(lbs): {encounter.weight_lbs}</p>
-            <p>Altura(inch): {encounter.height_inch}</p>
-            <p>Presión Arterial Sistólica: {encounter.systolic_bp}</p>
-            <p>Presión Arterial Diastólica: {encounter.diastolic_bp}</p>
-            <p>Ritmo Cardiaco: {encounter.heart_rate}</p>
-            <p>Respiraciones Por Minuto: {encounter.respirations_min}</p>
-            <p>Notas: {encounter.commenttxt}</p>
-          </div>
-        )}
       </div>
       }
       </div>
