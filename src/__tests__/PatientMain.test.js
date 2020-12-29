0a51c02c3acd5f48ef53eda36a44892d1f1f0150
wiebejordan <wiebe.jordan@yahoo.com>
2020-12-29 10:32:56 -0700

    writing tests

---
+++ src/__tests__/PatientMain.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {act, render, cleanup} from '@testing-library/react';
+import PatientMain from '../Components/Patient/PatientMain/PatientMain';
+import {MemoryRouter} from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import mockedAxios from '../__tests__/__mocks__/axios';
+import { Provider } from 'react-redux';
+import {store} from '../Redux/store';
+import patient from '../Components/Patient/PatientMain/PatientMain'
+
+afterEach(cleanup);
+
+it('makes an axios get call for patient info', async () => { 
+  const pat = patient[0];
+  let container;
+  jest
+      .spyOn(mockedAxios, 'get')
+      .mockImplementation(() => Promise.resolve({data}));
+  
+  
+  
+})
